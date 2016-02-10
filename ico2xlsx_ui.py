826728cf3f202fca84d496a8f987980f3cf0a7f5
Jason Tsai <jason@Jasons-MacBook.local>
2016-02-10 13:43:09 +0800

    GUI implemented.

--- ico2xlsx_ui.py
+++ ico2xlsx_ui.py
@@ -1,6 +1,6 @@
 import xlsxwriter
-from PIL import Image
-from Tkinter import Tk
+import PIL.Image
+from Tkinter import *
 from tkFileDialog import askopenfilename
 
 # Func Name: rgb2hex
@@ -17,23 +17,6 @@ def createXlsx(xlsxPath):
 
 	return workbook, worksheet
 
-def loadICO(imgPath):
-	try:
-		img = Image.open(imgPath)
-	except (NameError, AttributeError) as e:
-		print '[Error] ICO path is not correct'
-		exit()
-
-	print "Image type: " + img.format
-	print "Image mode: " + img.mode
-	print "Image size: " + str(img.size)
-
-	if img.format != 'ICO' and img.format != 'PNG':
-		print imgPath + ' is not a valid path.'
-		exit()
-
-	return img, img.size
-
 def convert(width, height, pixels, workbook, worksheet):
 	for y in range(height):
 		for x in range(width):
@@ -45,29 +28,104 @@ def convert(width, height, pixels, workbook, worksheet):
 			worksheet.write(y, x, '', format)
 			print 'x = %s, y = %s, RGBA = %s,%s,%s,%s , hex = %s' % (x, y, r, g, b, a, rgb2hex(r, g, b, a))
 
-# Open ICO
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-ICO_Path = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+def loadICO(_imgPath):
+	try:
+		img = PIL.Image.open(_imgPath)
+	except (NameError, AttributeError) as e:
+		print '[Error] ICO path is not correct'
+		# exit()
+	except IOError as e:
+		if str(e).find('cannot identify image file') != -1:
+			print _imgPath + ' is not a valid path.'
+			# exit()
 
-if ICO_Path == '':
-	print '[Error] ICO path is empty.'
-	exit()
+	print "Image type: " + img.format
+	print "Image mode: " + img.mode
+	print "Image size: " + str(img.size)
 
-icon, (width, height) = loadICO(ICO_Path)
-pixels = icon.convert('RGBA').load()
+	if img.format != 'ICO' and img.format != 'PNG':
+		print _imgPath + ' is not a valid path.'
+		# exit()
 
-# Create xlsx File
-print 'Creating xlsx file...'
-workbook, worksheet = createXlsx('hello.xlsx')
+	return img, img.size
 
-# Set column width to make it square
-worksheet.set_column(0, width-1, 1.9)
+class GUIDemo(Frame):
+	imgPath = ''
 
-# Convert ICO to HEX and Fill Cells
-print 'Converting...'
-convert(width, height, pixels, workbook, worksheet)
+	def __init__(self, master=None):
+		Frame.__init__(self, master)
+		self.grid()
+		self.createWidgets()
+ 
+	def createWidgets(self):
+		self.inputText = Label(self)
+		self.inputText["text"] = "ICO Path:"
+		self.inputText.grid(row=0, column=0)
+		self.inputField = Entry(self)
+		self.inputField["width"] = 50
+		self.inputField.grid(row=0, column=1, columnspan=6)
+ 
+		# self.outputText = Label(self)
+		# self.outputText["text"] = "Output:"
+		# self.outputText.grid(row=1, column=0)
+		# self.outputField = Entry(self)
+		# self.outputField["width"] = 50
+		# self.outputField.grid(row=1, column=1, columnspan=6)
+		 
+		self.load = Button(self)
+		self.load["text"] = "Load"
+		self.load.grid(row=2, column=5)
+		self.load["command"] =  self.loadMethod
+		
+		self.convert = Button(self)
+		self.convert["text"] = "Convert"
+		self.convert.grid(row=2, column=6)
+		self.convert["command"] =  self.convertMethod
+ 
+		self.displayText = Label(self)
+		self.displayText["text"] = ""
+		self.displayText.grid(row=3, column=0, columnspan=7)
+	  
+	def loadMethod(self):
+		self.imgPath = askopenfilename()
+		self.inputField.insert(END, self.imgPath) 
+ 
+	def convertMethod(self):
+		self.imgPath = self.inputField.get()
+		print 'imgPath = ' + self.imgPath
+		print 'entry = ' + self.inputField.get()
+		self.displayText["text"] = self.imgPath
 
-# Finish up
-workbook.close()
+		if self.imgPath == '':
+			self.displayText["text"] = '[Error] ICO path is empty.'
 
-print 'Done!'
+		icon, (width, height) = loadICO(self.imgPath)
+		pixels = icon.convert('RGBA').load()
+
+		# Create xlsx File
+		print 'Creating xlsx file...'
+		self.displayText["text"] = 'Creating xlsx file...'
+		workbook, worksheet = createXlsx('hello.xlsx')
+
+		# Set column width to make it square
+		worksheet.set_column(0, width-1, 1.9)
+
+		# Convert ICO to HEX and Fill Cells
+		print 'Converting...'
+		self.displayText["text"] = 'Converting...'
+		convert(width, height, pixels, workbook, worksheet)
+
+		# Finish up
+		workbook.close()
+
+		print 'Done!'
+		self.displayText["text"] = 'Done!'
+		# exit()
+
+if __name__ == '__main__':
+	
+	# Open ICO
+	root = Tk()
+	root.title("ico2xlsx Converter")
+	app = GUIDemo(master=root)
+	app.mainloop()
